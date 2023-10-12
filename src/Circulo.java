485e1c4314c6626ec640f98dfe1f2a9d83bb2996
Abrahamvazquez19 <147737860+Abrahamvazquez19@users.noreply.github.com>
2023-10-12 10:22:59 -0600

    Create Circulo.java

---
+++ src/Circulo.java
@@ -0,0 +1,12 @@
+Import java.util.Scanner;
+public class Circulo{
+  public static void main (String)[] args){
+  Scanner scanner = new Scanner(System.in);
+
+System.out.print("Ingresa el radio del circulo");
+double radio = Scanner.nextdouble();
+
+double area = math.PI* Math.pow(radio, 2);
+System.out.println("El area del circulo es :"+area);
+}
+}
