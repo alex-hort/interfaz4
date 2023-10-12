b80d8ac097cee5007d41ef1da69fd829f898403f
Larissa477 <141948541+Larissa477@users.noreply.github.com>
2023-10-12 10:12:29 -0600

    Update Ciadrado.java

--- src/Ciadrado.java
+++ src/Ciadrado.java
@@ -1,1 +1,29 @@
+public class Ciadrado {
+    private double lado1,lado2;
 
+    public Ciadrado(int lado1, int lado2){
+        this.lado1=lado1;
+        this.lado2=lado2;
+    }
+
+    public double getLado1() {
+        return lado1;
+    }
+
+    public void setLado1(int lado1) {
+        this.lado1 = lado1;
+    }
+
+    public double getLado2() {
+        return lado2;
+    }
+
+    public void setLado2(int lado2) {
+        this.lado2 = lado2;
+    }
+  
+    float obtenerArea(){
+       float area= getLado1()*getLado2();
+       return area;
+    }
+}
